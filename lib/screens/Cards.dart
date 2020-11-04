207f7b7cf064b1536133701dd843547f072a98ca
albus12cb11 <cbarbault.ing2022@esaip.org>
2020-11-04 12:10:42 +0100

    design card 1st step

--- lib/screens/Cards.dart
+++ lib/screens/Cards.dart
@@ -9,10 +9,103 @@ class RowCards extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Card(),
-        Card(),
-        Card(),
+        ResultCard(),
+        ResultCard(),
+        ResultCard(),
       ],
+    );
+  }
+}
+
+class ResultCard extends StatelessWidget {
+  const ResultCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Recto(2000, 93);
+  }
+}
+
+// ignore: must_be_immutable
+class Recto extends StatelessWidget {
+  int pop;
+  int score;
+
+  Recto(
+    this.pop,
+    this.score,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text('Commune'),
+              Text('$pop'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Score'),
+              Text('$score'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class Verso extends StatelessWidget {
+  int accesNum;
+  int accesInfo;
+  int compAdmin;
+  int compNum;
+
+  Verso(
+    this.accesNum,
+    this.accesInfo,
+    this.compAdmin,
+    this.compNum,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text('Accès Numérique'),
+              Text('$accesNum'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Accès Information'),
+              Text('$accesInfo'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Compétences administratives'),
+              Text('$compAdmin'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Compétences numériques'),
+              Text('$compNum'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
